ad7a02727a9dfd8db40a939e59be4188455b5350
admin <admin@New000001.local>
2018-03-22 21:35:01 +0000

    added versioning

--- jenkins_slave/provision.sh
+++ jenkins_slave/provision.sh
@@ -1,1 +1,10 @@
+wget https://releases.hashicorp.com/packer/1.2.1/packer_1.2.1_linux_amd64.zip?_ga=2.121080584.1660155545.1521728395-1981876141.1518449816
+mv packer_1.2.1_linux_amd64.zip?_ga=2.121080584.1660155545.1521728395-1981876141.1518449816 packer
+yum install -y unzip
+pwd
+ls -ltr
+unzip -o packer
+ls -ltr
+chmod 777 packer
+cp packer /var/tmp/
 sed -i -e '/<servers>/r /var/tmp/maven_config.xml' /etc/maven/settings.xml
